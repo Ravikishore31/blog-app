045e23321ee1684f388fd037ade72f1a88e561b3
Ravikishore31 <rkishore.adabala@gmail.com>
2023-04-18 11:00:00 +0530

    home updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,7 +12,9 @@ const Home = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(`/posts${cat}`);
+        const res = await axios.get(
+          `https://leafy-crostata-e5a46f.netlify.app//posts${cat}`
+        );
         setPosts(res.data);
       } catch (err) {
         console.log(err);
